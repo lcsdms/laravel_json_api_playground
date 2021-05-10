a10b7607ed5ca44f5cf240b8ded695df4c49c5cd
Lucas Dimas <lucasdcavalcante@gmail.com>
2021-05-10 07:47:49 -0300

    Randomizing seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            PersonSeeder::class,
-            CompanySeeder::class
+            CompanySeeder::class,
+            PersonSeeder::class
         ]);
     }
 }
--- database/seeders/PersonSeeder.php
+++ database/seeders/PersonSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Email;
 use App\Models\Person;
 use Illuminate\Database\Seeder;
 
@@ -14,8 +15,10 @@ class PersonSeeder extends Seeder
      */
     public function run()
     {
-        Person::factory()
-            ->count(20)
-            ->create();
+        Person::factory()->count(20)->create();
+        Person::factory()->count(20)
+            ->create()->each(function($person){
+               $person->entity->emails()->saveMany(Email::factory()->count(random_int(1,3))->make());
+            });
     }
 }
